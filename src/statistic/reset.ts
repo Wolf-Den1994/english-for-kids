1ed6158e30732e0a2483120e4dbb15e29dd93bb0
Denis <wolf-den@tut.by>
2021-07-13 23:22:07 +0300

    fix: mutate array reset

--- src/statistic/reset.ts
+++ src/statistic/reset.ts
@@ -4,18 +4,20 @@ import { Order } from '../utils/enums';
 import { IFullCards } from '../utils/interfaces';
 import { renderStatistic } from './render-statistic';
 
-const cleanArr = (arr: IFullCards[]): void => {
-  arr.forEach((item) => {
-    item.train = 0;
-    item.play = 0;
-    item.errors = 0;
-    item.answers = 0;
-    item.percent = 0;
-  });
+const cleanArr = (arr: IFullCards[]) => {
+  const resettedArray: IFullCards[] = arr.map((item) => ({
+    ...item,
+    train: 0,
+    play: 0,
+    errors: 0,
+    answers: 0,
+    percent: 0,
+  }));
+  return resettedArray;
 };
 
 export const resetStatistic = (): void => {
-  cleanArr(fullCards);
-  localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(fullCards));
-  renderStatistic(fullCards, Order.DESC);
+  const resettedArray = cleanArr(fullCards);
+  localStorage.setItem(NAME_LOCALSTORAGE, JSON.stringify(resettedArray));
+  renderStatistic(resettedArray, Order.DESC);
 };
